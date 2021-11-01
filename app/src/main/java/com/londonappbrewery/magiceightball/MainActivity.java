e23be59a52c6d35a92fb1f2b69a410ce13c0ed9a
chance-b <chance.bloom7@gmail.com>
2021-10-31 18:15:19 -0700

    completed the challenge

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,14 @@
 package com.londonappbrewery.magiceightball;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //link askButton(button) and ballDisplay(img) to java code using findViewById
+        final ImageView ballDisplay = findViewById(R.id.ballDisplay);
+        Button askButton = findViewById(R.id.askButton);
+
+        TextView banner = findViewById(R.id.textView_banner);
+        banner.setShadowLayer(8,0,0, Color.BLACK);
+
+        //int arr to store 8ball images
+        final int[] ballArr = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        //onclick listener for ask button
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //create rand int to use for ball display
+                Random randInt = new Random();
+                int num = randInt.nextInt(5);
+
+                //set ballDisplay to be random
+                ballDisplay.setImageResource(ballArr[num]);
+            }
+        });
     }
 }
